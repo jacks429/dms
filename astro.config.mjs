dcda7a685ac790b447013a672a566812b2cdb17c
jacks429 <64680540+jacks429@users.noreply.github.com>
2024-04-13 01:37:09 -0500

    DENO INT :clown_face:

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,28 +2,20 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import db from "@astrojs/db";
-import node from "@astrojs/node";
-const DEV_PORT = 2121;
+
+import deno from '@astrojs/deno';
+
 
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.CI ? 'https://themesberg.github.io' : `http://localhost:${DEV_PORT}`,
-  base: process.env.CI ? '/flowbite-astro-admin-dashboard' : undefined,
-  // output: 'server',
 
-  /* Like Vercel, Netlify,… Mimicking for dev. server */
-  // trailingSlash: 'always',
 
-  server: {
-    /* Dev. server only */
-    port: DEV_PORT
-  },
+
   integrations: [
   //
   sitemap(), tailwind(), db()],
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  output: 'server',
+  adapter: deno(),
+  
 });
