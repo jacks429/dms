09bcd57f0d5904ee59bfc0e507df1865bec0f46b
jacks429 <64680540+jacks429@users.noreply.github.com>
2024-04-13 03:05:08 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,26 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import db from "@astrojs/db";
-
-import deno from '@astrojs/deno';
-
+const DEV_PORT = 2121;
 
 
 // https://astro.build/config
 export default defineConfig({
+  site: process.env.CI ? 'https://themesberg.github.io' : `http://localhost:${DEV_PORT}`,
+  base: process.env.CI ? '/flowbite-astro-admin-dashboard' : undefined,
+  // output: 'server',
 
+  /* Like Vercel, Netlify,… Mimicking for dev. server */
+  // trailingSlash: 'always',
 
-
+  server: {
+    /* Dev. server only */
+    port: DEV_PORT
+  },
   integrations: [
+
+
   //
   sitemap(), tailwind(), db()],
-  output: 'server',
-  adapter: deno(),
   
 });
