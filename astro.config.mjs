012b6feea562cdff5e68c50c1dd27161b29d9f97
jacks429 <64680540+jacks429@users.noreply.github.com>
2024-05-10 18:13:06 -0500

    added cloudflare and wrangler file

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import cloudflare from "@astrojs/cloudflare";
+
 const DEV_PORT = 2121;
 
 
