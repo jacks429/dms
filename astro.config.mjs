9c1b1e1548d31df5ca08fea5c6937c182bd1f15e
jacks429 <64680540+jacks429@users.noreply.github.com>
2024-05-10 20:51:26 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,27 +3,37 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import cloudflare from '@astrojs/cloudflare'
 
-const DEV_PORT = 2121;
-
-
-// https://astro.build/config
 export default defineConfig({
-  site: process.env.CI ? 'https://themesberg.github.io' : `http://localhost:${DEV_PORT}`,
-  base: process.env.CI ? '/flowbite-astro-admin-dashboard' : undefined,
-  // output: 'server',
-
-  /* Like Vercel, Netlify,… Mimicking for dev. server */
-  // trailingSlash: 'always',
-
-  server: {
-    /* Dev. server only */
-    port: DEV_PORT
-  },
-  integrations: [
-
-
-  //
-  sitemap(), tailwind()],
   output: "server",
-  adapter: cloudflare()
-});
+  adapter: cloudflare(),
+	integrations: [
+		sitemap(), 
+		tailwind()],
+
+	});
+	
+
+// const DEV_PORT = 2121;
+
+
+// // https://astro.build/config
+// export default defineConfig({
+//   site: process.env.CI ? 'https://themesberg.github.io' : `http://localhost:${DEV_PORT}`,
+//   base: process.env.CI ? '/flowbite-astro-admin-dashboard' : undefined,
+//   // output: 'server',
+
+//   /* Like Vercel, Netlify,… Mimicking for dev. server */
+//   // trailingSlash: 'always',
+
+//   server: {
+//     /* Dev. server only */
+//     port: DEV_PORT
+//   },
+//   integrations: [
+
+
+  
+//   sitemap(), tailwind()],
+//   output: "server",
+//   adapter: cloudflare()
+// });
